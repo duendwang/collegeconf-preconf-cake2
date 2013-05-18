eb1fa6c5f8f11a6987b91ae58b7e01c6df26ad9a
davidw <davidw@e737e107-2d7c-4894-a2a0-2c16c2e59d14>
2013-05-18 00:32:23 +0000

    Campus code allow empty

--- Model/Campus.php
+++ Model/Campus.php
@@ -35,7 +35,7 @@ class Campus extends AppModel {
 			'alphanumeric' => array(
 				'rule' => array('alphanumeric'),
 				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
+				'allowEmpty' => true,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
